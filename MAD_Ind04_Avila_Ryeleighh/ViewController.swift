004e2bee092723c673a508e3ac3a7d9a323ff73c
RyeleighAvila <rye03_a@icloud.com>
2024-04-27 14:51:45 -0500

    Initial Commit

---
+++ MAD_Ind04_Avila_Ryeleighh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD_Ind04_Avila_Ryeleighh
+//
+//  Created by Ryeleigh Avila on 4/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
